791bfe5b9b00f0c220da584ecb4cde8ea39bd37f
Jha <atul.a.jha@capgemini.com>
2023-01-18 11:21:18 +0530

    Shifting Code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Counter from './components/Counter';
 import Header from './components/Header'
 import Authentication from './components/Auth'
+import UserProfile from './components/UserProfile'
+import {useSelector} from 'react-redux'
 
 function App() {
+  const isAuth = useSelector(state => state.auth.isAuthenticated)
   return (
     <>
       <Header />
-      <Authentication />
+      {!isAuth && <Authentication />}
+      {isAuth && <UserProfile />}
       <Counter />
     </>
   );
